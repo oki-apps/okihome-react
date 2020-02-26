3c207145e111e53b7ea66548d3730dd403cb4a46
Simon HEGE <simon.hege@gmail.com>
2020-02-26 10:20:51 +0100

    Update userManager.js

--- src/userManager.js
+++ src/userManager.js
@@ -1,13 +1,12 @@
 import { createUserManager } from 'redux-oidc';
 
 const userManagerConfig = {
-  client_id: '88998355665-78cac36apbrms0gcvbht5gsv508ldakb.apps.googleusercontent.com',
+  client_id: 'okihome-app',
   redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/callback`,
-  response_type: 'token id_token',
-  scope: 'openid profile email',
-  authority: 'https://accounts.google.com',
-  filterProtocolClaims: true,
-  loadUserInfo: true,
+  response_type: 'id_token token',
+  scope: 'openid profile email offline_access',
+  authority: 'https://login.xdbsoft.com/',
+  loadUserInfo: false,
 };
 /*
 const userManagerConfig = {
